e2bd0c98c2c85639846b79fdf41224601a81594e
Seb-Maraszek <sebastian.maraszek0@gmail.com>
2020-04-22 16:49:16 +0200

    Funkcja która pobiera z google API dane dotyczące wybranego kanału na YT

--- Data_Collector.py
+++ Data_Collector.py
@@ -1,18 +1,20 @@
 import urllib.request
 import json
 
+# lista kanałów których dane będą zbierane
+channels_toCheck = ["DisStream", "liebner12", "PewDiePie", "UFC", "WilliamsF1TV", "BadzmyPowazni", "mietczynski",
+                    "KSW", "OwcaWK", "Nervarien"]
+
 
 def get_data(name):
     """Get info about YT channel, and return dictionary."""
-
     key = "AIzaSyCGpvMVtDdvBVboHjxRDsWdx4wXTkQPf_0"
+
     data = urllib.request.urlopen(
         "https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername=" + name + "&key=" + key).read()
+
     subs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
     views = json.loads(data)["items"][0]["statistics"]["viewCount"]
     videos = json.loads(data)["items"][0]["statistics"]["videoCount"]
-    data = {'subsriptions': int(subs), 'videos': int(videos), 'views': int(views)}
+    data = {'name': name, 'subscriptions': int(subs), 'videos': int(videos), 'views': int(views)}
     return data
-
-
-print(get_data("DisStream"))
